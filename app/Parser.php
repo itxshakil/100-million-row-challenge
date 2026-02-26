8040f9c127be24730024dad1b918c24be7086677
Shakil Alam <30234430+itxshakil@users.noreply.github.com>
2026-02-26 12:47:30 +0530

    Tried simplest way.

--- app/Parser.php
+++ app/Parser.php
@@ -3,11 +3,58 @@
 namespace App;
 
 use Exception;
+use RuntimeException;
 
 final class Parser
 {
+    /**
+     * @throws Exception
+     */
     public function parse(string $inputPath, string $outputPath): void
     {
-        throw new Exception('TODO');
+        $handle = fopen($inputPath, 'rb');
+        if (!$handle) {
+            throw new RuntimeException("Could not open input file: $inputPath");
+        }
+
+        $visits = [];
+
+        while (($line = fgets($handle)) !== false) {
+            $line = trim($line);
+            if (empty($line)) {
+                continue;
+            }
+
+            $parts = explode(',', $line);
+            if (count($parts) < 2) {
+                continue;
+            }
+
+            [$url, $timestamp] = $parts;
+
+            $path = parse_url($url, PHP_URL_PATH);
+            $date = substr($timestamp, 0, 10); //YYYY-MM-DD
+
+            if (!isset($visits[$path])) {
+                $visits[$path] = [];
+            }
+
+            if (!isset($visits[$path][$date])) {
+                $visits[$path][$date] = 0;
+            }
+
+            $visits[$path][$date]++;
+        }
+
+        fclose($handle);
+
+        foreach ($visits as $path => $dates) {
+            ksort($visits[$path]);
+        }
+
+        file_put_contents(
+            $outputPath,
+            json_encode($visits, JSON_PRETTY_PRINT)
+        );
     }
 }
